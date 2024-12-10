42a273e2987fbe5ee035d5052d18cbbb9374e60c
Przemysław Sosna <43684335+SosenWiosen@users.noreply.github.com>
2024-12-10 23:39:45 +0100

    clean expedriments file and update get_top.py

--- ana-classifier/experiments.py
+++ ana-classifier/experiments.py
@@ -4,14 +4,10 @@ from tensorflow.python.keras.callbacks import EarlyStopping
 from tensorflow.keras.optimizers.legacy import Adam
 from model_tester.test_model import test_model
 
+
 data_augmentation = tf.keras.Sequential([
-    # tf.keras.layers.Rescaling(1. / 255),
     tf.keras.layers.RandomRotation(0.1),
-    # You can add more augmentations if needed
-    # tf.keras.layers.RandomZoom(0.15),
-    # tf.keras.layers.RandomWidth(0.2),
-    # tf.keras.layers.RandomHeight(0.2),
-    # tf.keras.layers.RandomShear(0.15),
+    tf.keras.layers.RandomContrast(0.05),
     tf.keras.layers.RandomFlip("horizontal"),
     tf.keras.layers.RandomFlip("vertical"),
 ])
@@ -25,183 +21,13 @@ early_stopping = EarlyStopping(
     restore_best_weights=True
 )
 
-# test_model(save_path, dst_path, "efficientnetv2b0", data_augmentation, optimizer=optimizer, early_stopping=early_stopping)
-# dst_path = "/Users/sosen/UniProjects/eng-thesis/data/AC8-combined/CROPPED/NON-STED"
-# save_path = "/Users/sosen/UniProjects/eng-thesis/experiments/basic-adamw/combined"
-#
-# test_model(save_path, dst_path, "efficientnetv2b0", data_augmentation, optimizer=AdamW(), early_stopping=early_stopping,
-#            attempt_name="efficientnetv2b0", finetune=True, finetune_optimizer=AdamW(learning_rate=1e-5),
-#            finetune_early_stopping=early_stopping)
+
 
 dst_path = "/Users/sosen/UniProjects/eng-thesis/data/datasets-split/AC8-combined/NON-STED"
 save_path = "/Users/sosen/UniProjects/eng-thesis/experiments/basic-adam-non-sted"
-
-# test_model(save_path, dst_path, "efficientnetv2b0", data_augmentation, optimizer="adam", early_stopping=early_stopping,
-#            attempt_name="efficientnetv2b0")
-# test_model(save_path, dst_path, "efficientnetv2b1", data_augmentation, optimizer="adam", early_stopping=early_stopping,
-#            attempt_name="efficientnetv2b1")
-# test_model(save_path, dst_path, "efficientnetv2b2", data_augmentation, optimizer="adam", early_stopping=early_stopping,
-#            attempt_name="efficientnetv2b2")
-# test_model(save_path, dst_path, "efficientnetv2b3", data_augmentation, optimizer="adam", early_stopping=early_stopping,
-#            attempt_name="efficientnetv2b3")
-# test_model(save_path, dst_path, "efficientnetv2s", data_augmentation, optimizer="adam", early_stopping=early_stopping,
-#            attempt_name="efficientnetv2s")
-# test_model(save_path, dst_path, "efficientnetv2m", data_augmentation, optimizer="adam", early_stopping=early_stopping,
-#            attempt_name="efficientnetv2m")
-# test_model(save_path, dst_path, "efficientnetv2l", data_augmentation, optimizer="adam", early_stopping=early_stopping,
-#            attempt_name="efficientnetv2l")
-# test_model(save_path, dst_path, "resnet50v2", data_augmentation, optimizer="adam", early_stopping=early_stopping,
-#            attempt_name="resnet50v2")
-# test_model(save_path, dst_path, "resnet101v2", data_augmentation, optimizer="adam", early_stopping=early_stopping,
-#            attempt_name="resnet101v2")
-# test_model(save_path, dst_path, "resnet152v2", data_augmentation, optimizer="adam", early_stopping=early_stopping,
-#            attempt_name="resnet152v2")
-# test_model(save_path, dst_path, "densenet121", data_augmentation, optimizer="adam", early_stopping=early_stopping,
-#            attempt_name="densenet121")
-# test_model(save_path, dst_path, "densenet169", data_augmentation, optimizer="adam", early_stopping=early_stopping,
-#            attempt_name="densenet169")
-# test_model(save_path, dst_path, "densenet201", data_augmentation, optimizer="adam", early_stopping=early_stopping,
-#            attempt_name="densenet201")
-# test_model(save_path, dst_path, "mobilenetv2", data_augmentation, optimizer="adam", early_stopping=early_stopping,
-#            attempt_name="mobilenetv2")
-# test_model(save_path, dst_path, "mobilenetv3small", data_augmentation, optimizer="adam", early_stopping=early_stopping,
-#            attempt_name="mobilenetv3small")
-# test_model(save_path, dst_path, "mobilenetv3large", data_augmentation, optimizer="adam", early_stopping=early_stopping,
-#            attempt_name="mobilenetv3large")
-test_model(save_path, dst_path, "vgg16", data_augmentation, optimizer="adam", early_stopping=early_stopping,
-           attempt_name="vgg16")
-test_model(save_path, dst_path, "vgg19", data_augmentation, optimizer="adam", early_stopping=early_stopping,
-           attempt_name="vgg19")
+test_model(save_path, dst_path, "efficientnetv2b0", data_augmentation, optimizer=AdamW(), early_stopping=early_stopping,
+           attempt_name="efficientnetv2b0", finetune=True, finetune_optimizer=AdamW(learning_rate=1e-5),
+           finetune_early_stopping=early_stopping)
 dst_path = "/Users/sosen/UniProjects/eng-thesis/data/datasets-split/AC8-combined/STED"
 save_path = "/Users/sosen/UniProjects/eng-thesis/experiments/basic-adam-sted"
 
-test_model(save_path, dst_path, "efficientnetv2b0", data_augmentation, optimizer="adam", early_stopping=early_stopping,
-           attempt_name="efficientnetv2b0")
-test_model(save_path, dst_path, "efficientnetv2b1", data_augmentation, optimizer="adam", early_stopping=early_stopping,
-           attempt_name="efficientnetv2b1")
-test_model(save_path, dst_path, "efficientnetv2b2", data_augmentation, optimizer="adam", early_stopping=early_stopping,
-           attempt_name="efficientnetv2b2")
-test_model(save_path, dst_path, "efficientnetv2b3", data_augmentation, optimizer="adam", early_stopping=early_stopping,
-           attempt_name="efficientnetv2b3")
-test_model(save_path, dst_path, "efficientnetv2s", data_augmentation, optimizer="adam", early_stopping=early_stopping,
-           attempt_name="efficientnetv2s")
-test_model(save_path, dst_path, "efficientnetv2m", data_augmentation, optimizer="adam", early_stopping=early_stopping,
-           attempt_name="efficientnetv2m")
-test_model(save_path, dst_path, "efficientnetv2l", data_augmentation, optimizer="adam", early_stopping=early_stopping,
-           attempt_name="efficientnetv2l")
-test_model(save_path, dst_path, "resnet50v2", data_augmentation, optimizer="adam", early_stopping=early_stopping,
-           attempt_name="resnet50v2")
-test_model(save_path, dst_path, "resnet101v2", data_augmentation, optimizer="adam", early_stopping=early_stopping,
-           attempt_name="resnet101v2")
-test_model(save_path, dst_path, "resnet152v2", data_augmentation, optimizer="adam", early_stopping=early_stopping,
-           attempt_name="resnet152v2")
-test_model(save_path, dst_path, "densenet121", data_augmentation, optimizer="adam", early_stopping=early_stopping,
-           attempt_name="densenet121")
-test_model(save_path, dst_path, "densenet169", data_augmentation, optimizer="adam", early_stopping=early_stopping,
-           attempt_name="densenet169")
-test_model(save_path, dst_path, "densenet201", data_augmentation, optimizer="adam", early_stopping=early_stopping,
-           attempt_name="densenet201")
-test_model(save_path, dst_path, "mobilenetv2", data_augmentation, optimizer="adam", early_stopping=early_stopping,
-           attempt_name="mobilenetv2")
-test_model(save_path, dst_path, "mobilenetv3small", data_augmentation, optimizer="adam", early_stopping=early_stopping,
-           attempt_name="mobilenetv3small")
-test_model(save_path, dst_path, "mobilenetv3large", data_augmentation, optimizer="adam", early_stopping=early_stopping,
-           attempt_name="mobilenetv3large")
-test_model(save_path, dst_path, "vgg16", data_augmentation, optimizer="adam", early_stopping=early_stopping,
-           attempt_name="vgg16")
-test_model(save_path, dst_path, "vgg19", data_augmentation, optimizer="adam", early_stopping=early_stopping,
-           attempt_name="vgg19")
-
-dst_path = "/Users/sosen/UniProjects/eng-thesis/data/datasets-split/AC8-combined/NON-STED"
-save_path = "/Users/sosen/UniProjects/eng-thesis/experiments/basic-adam-non-sted"
-test_model(save_path, dst_path, "inceptionv3", data_augmentation, optimizer="adam", early_stopping=early_stopping,
-           attempt_name="inceptionv3")
-test_model(save_path, dst_path, "inceptionresnetv2", data_augmentation, optimizer="adam", early_stopping=early_stopping,
-           attempt_name="inceptionresnetv2")
-test_model(save_path, dst_path, "nasnetlarge", data_augmentation, optimizer="adam", early_stopping=early_stopping,
-           attempt_name="nasnetlarge")
-test_model(save_path, dst_path, "nasnetmobile", data_augmentation, optimizer="adam", early_stopping=early_stopping,
-           attempt_name="nasnetmobile")
-test_model(save_path, dst_path, "xception", data_augmentation, optimizer="adam", early_stopping=early_stopping,
-           attempt_name="xception")
-
-dst_path = "/Users/sosen/UniProjects/eng-thesis/data/datasets-split/AC8-combined/STED"
-save_path = "/Users/sosen/UniProjects/eng-thesis/experiments/basic-adam-sted"
-test_model(save_path, dst_path, "inceptionv3", data_augmentation, optimizer="adam", early_stopping=early_stopping,
-           attempt_name="inceptionv3")
-test_model(save_path, dst_path, "inceptionresnetv2", data_augmentation, optimizer="adam", early_stopping=early_stopping,
-           attempt_name="inceptionresnetv2")
-test_model(save_path, dst_path, "nasnetlarge", data_augmentation, optimizer="adam", early_stopping=early_stopping,
-           attempt_name="nasnetlarge")
-test_model(save_path, dst_path, "nasnetmobile", data_augmentation, optimizer="adam", early_stopping=early_stopping,
-           attempt_name="nasnetmobile")
-test_model(save_path, dst_path, "xception", data_augmentation, optimizer="adam", early_stopping=early_stopping,
-           attempt_name="xception")
-
-# test_model(save_path, dst_path, "resnet50v2", data_augmentation, optimizer=Adam(), early_stopping=early_stopping,
-#            attempt_name="resnet50v2", finetune=True, finetune_optimizer=Adam(learning_rate=1e-5),
-#            finetune_early_stopping=early_stopping)
-# test_model(save_path, dst_path, "densenet121", data_augmentation, optimizer=Adam(), early_stopping=early_stopping,
-#            attempt_name="densenet121", finetune=True, finetune_optimizer=Adam(learning_rate=1e-5),
-#            finetune_early_stopping=early_stopping)
-# test_model(save_path, dst_path, "efficientnetb0", data_augmentation, optimizer=Adam(), early_stopping=early_stopping,
-#            attempt_name="efficientnetb0", finetune=True, finetune_optimizer=Adam(learning_rate=1e-5),
-#            finetune_early_stopping=early_stopping)
-# test_model(save_path, dst_path, "mobilenetv2", data_augmentation, optimizer=Adam(), early_stopping=early_stopping,
-#            attempt_name="mobilenetv2", finetune=True, finetune_optimizer=Adam(learning_rate=1e-5),
-#            finetune_early_stopping=early_stopping)
-
-# dst_path = "/Users/sosen/UniProjects/eng-thesis/data/datasets-split/AC8-combined/NON-STED"
-#
-# test_model(save_path, dst_path, "efficientnetv2b0", data_augmentation, optimizer=Adam(), early_stopping=early_stopping,
-#            attempt_name="efficientnetv2b0 STED", finetune=True, finetune_optimizer=Adam(learning_rate=1e-5),
-#            finetune_early_stopping=early_stopping)
-# test_model(save_path, dst_path, "resnet50v2", data_augmentation, optimizer=Adam(), early_stopping=early_stopping,
-#            attempt_name="resnet50v2  STED", finetune=True, finetune_optimizer=Adam(learning_rate=1e-5),
-#            finetune_early_stopping=early_stopping)
-# test_model(save_path, dst_path, "densenet121", data_augmentation, optimizer=Adam(), early_stopping=early_stopping,
-#            attempt_name="densenet121 STED", finetune=True, finetune_optimizer=Adam(learning_rate=1e-5),
-#            finetune_early_stopping=early_stopping)
-# test_model(save_path, dst_path, "efficientnetb0", data_augmentation, optimizer=Adam(), early_stopping=early_stopping,
-#            attempt_name="efficientnetb0 STED", finetune=True, finetune_optimizer=Adam(learning_rate=1e-5),
-#            finetune_early_stopping=early_stopping)
-# test_model(save_path, dst_path, "mobilenetv2", data_augmentation, optimizer=Adam(), early_stopping=early_stopping,
-#            attempt_name="mobilenetv2 STED", finetune=True, finetune_optimizer=Adam(learning_rate=1e-5),
-#            finetune_early_stopping=early_stopping)
-#
-# save_path = "/Users/sosen/UniProjects/eng-thesis/experiments/basic-adam/rejected"
-# dst_path = "/Users/sosen/UniProjects/eng-thesis/data/datasets-split/AC8-rejected/NON-STED"
-#
-# test_model(save_path, dst_path, "efficientnetv2b0", data_augmentation, optimizer=Adam(), early_stopping=early_stopping,
-#            attempt_name="efficientnetv2b0", finetune=True, finetune_optimizer=Adam(learning_rate=1e-5),
-#            finetune_early_stopping=early_stopping)
-# test_model(save_path, dst_path, "resnet50v2", data_augmentation, optimizer=Adam(), early_stopping=early_stopping,
-#            attempt_name="resnet50v2", finetune=True, finetune_optimizer=Adam(learning_rate=1e-5),
-#            finetune_early_stopping=early_stopping)
-# test_model(save_path, dst_path, "densenet121", data_augmentation, optimizer=Adam(), early_stopping=early_stopping,
-#            attempt_name="densenet121", finetune=True, finetune_optimizer=Adam(learning_rate=1e-5),
-#            finetune_early_stopping=early_stopping)
-# test_model(save_path, dst_path, "efficientnetb0", data_augmentation, optimizer=Adam(), early_stopping=early_stopping,
-#            attempt_name="efficientnetb0", finetune=True, finetune_optimizer=Adam(learning_rate=1e-5),
-#            finetune_early_stopping=early_stopping)
-# test_model(save_path, dst_path, "mobilenetv2", data_augmentation, optimizer=Adam(), early_stopping=early_stopping,
-#            attempt_name="mobilenetv2", finetune=True, finetune_optimizer=Adam(learning_rate=1e-5),
-#            finetune_early_stopping=early_stopping)
-#
-# dst_path = "/Users/sosen/UniProjects/eng-thesis/data/datasets-split/AC8-rejected/STED"
-#
-# test_model(save_path, dst_path, "efficientnetv2b0", data_augmentation, optimizer=Adam(), early_stopping=early_stopping,
-#            attempt_name="efficientnetv2b0 STED", finetune=True, finetune_optimizer=Adam(learning_rate=1e-5),
-#            finetune_early_stopping=early_stopping)
-# test_model(save_path, dst_path, "resnet50v2", data_augmentation, optimizer=Adam(), early_stopping=early_stopping,
-#            attempt_name="resnet50v2  STED", finetune=True, finetune_optimizer=Adam(learning_rate=1e-5),
-#            finetune_early_stopping=early_stopping)
-# test_model(save_path, dst_path, "densenet121", data_augmentation, optimizer=Adam(), early_stopping=early_stopping,
-#            attempt_name="densenet121 STED", finetune=True, finetune_optimizer=Adam(learning_rate=1e-5),
-#            finetune_early_stopping=early_stopping)
-# test_model(save_path, dst_path, "efficientnetb0", data_augmentation, optimizer=Adam(), early_stopping=early_stopping,
-#            attempt_name="efficientnetb0 STED", finetune=True, finetune_optimizer=Adam(learning_rate=1e-5),
-#            finetune_early_stopping=early_stopping)
-# test_model(save_path, dst_path, "mobilenetv2", data_augmentation, optimizer=Adam(), early_stopping=early_stopping,
-#            attempt_name="mobilenetv2 STED", finetune=True, finetune_optimizer=Adam(learning_rate=1e-5),
-#            finetune_early_stopping=early_stopping)
--- ana-classifier/model_tester/model/get_top.py
+++ ana-classifier/model_tester/model/get_top.py
@@ -1,21 +1,30 @@
 from tensorflow.keras import layers
+import tensorflow as tf
 
 
-def get_top(base_model_output, num_classes ,top, dropout_rate):
-    outputs = None
-    if top == "avgpool":
-        x = layers.GlobalAveragePooling2D(name="avg_pool")(base_model_output)
-        x = layers.BatchNormalization()(x)
-        x = layers.Dropout(dropout_rate, name="top_dropout")(x)
-        outputs = layers.Dense(num_classes, activation="softmax", name="pred")(x)
-    elif top == "maxpool":
-        x = layers.GlobalMaxPooling2D(name="max_pool")(base_model_output)
-        x = layers.BatchNormalization()(x)
+def get_top(base_model_output, num_classes, top, dropout_rate):
+    x = None
+    # Check if a string (identifier for a predefined architecture) was passed
+    if isinstance(top, str):
+        if top == "avgpool":
+            x = layers.GlobalAveragePooling2D(name="avg_pool")(base_model_output)
+            x = layers.BatchNormalization()(x)
+            x = layers.Dropout(dropout_rate, name="top_dropout")(x)
+        elif top == "maxpool":
+            x = layers.GlobalMaxPooling2D(name="max_pool")(base_model_output)
+            x = layers.BatchNormalization()(x)
+            x = layers.Dropout(dropout_rate, name="top_dropout")(x)
+        elif top == "flatten_dense":
+            x = layers.Flatten()(base_model_output)
+            x = layers.Dense(1000, activation='relu')(x)
+    # Check if a TensorFlow Layer or model was passed
+    elif isinstance(top, (tf.keras.layers.Layer, tf.keras.Model)):
+        x = top(base_model_output)
 
-        x = layers.Dropout(dropout_rate, name="top_dropout")(x)
+    # Append the final Dense layer with softmax activation for classification
+    if x is not None:
         outputs = layers.Dense(num_classes, activation="softmax", name="pred")(x)
-    elif top == "flatten_dense":
-        x = layers.Flatten()(base_model_output)
-        x = layers.Dense(1000, activation='relu')(x)
-        outputs = layers.Dense(num_classes, activation="softmax", name="pred")(x)
-    return outputs
+        return outputs
+    else:
+        raise ValueError("Invalid value for 'top'. Must be either a string identifier or a TensorFlow Layer/Model.")
+
