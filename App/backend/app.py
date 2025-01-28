708c162b2504067bcf59840efabf2a585c5e8a19
Przemysław Sosna <przemek.sosna@onet.pl>
2025-01-28 14:36:30 +0000

    refine

--- App/backend/app.py
+++ App/backend/app.py
@@ -5,5 +5,5 @@ from app import create_app
 app = create_app()
 
 if __name__ == '__main__':
-    port = int(os.getenv("PORT", 5001))  # Default to port 5000 if PORT isn't set
-    app.run(debug=True, port=port)
+    port = int(os.getenv("PORT", 5002))  # Default to port 5000 if PORT isn't set
+    app.run(debug=True, host='0.0.0.0', port=port)
